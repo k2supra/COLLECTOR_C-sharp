f342a2cbabc525ec452324e9360fb4d3b4661563
k2supra <andrijkorolevic@gmail.com>
2024-04-17 17:50:09 +0300

    .

--- Entering/ex_1/Program.cs
+++ Entering/ex_1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,24 @@ namespace ex_1
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter a number between 1 to 100: ");
+            byte number = Convert.ToByte(Console.ReadLine());
+            if (number % 3 == 0 && number % 5 != 0) 
+            {
+                Console.WriteLine("Fizz"); 
+            }
+            else if (number % 3 != 0 && number % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else if (number % 3 == 0 && number % 5 == 0)
+            {
+                Console.WriteLine("Fizz Buzz");
+            }
+            else
+            {
+                Console.WriteLine(number);
+            }
         }
     }
 }
