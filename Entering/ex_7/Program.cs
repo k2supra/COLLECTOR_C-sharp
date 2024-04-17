07cbbaa4977e671b94342cc83b0c3449d7be8c13
k2supra <andrijkorolevic@gmail.com>
2024-04-17 21:19:31 +0300

    .

---
+++ Entering/ex_7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 2 numbers: ");
+            byte num1 = Convert.ToByte(Console.ReadLine());
+            byte num2 = Convert.ToByte(Console.ReadLine());
+
+            if (num1 > num2) 
+            {
+                byte temp = num2;
+                num2 = num1;
+                num1 = temp;
+            }
+            for (int i = num1; i < num2; i++)
+            {
+                if (i % 2 == 0) { Console.WriteLine(i + " "); }
+            }
+        }
+    }
+}
