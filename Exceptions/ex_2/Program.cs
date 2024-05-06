51575de2fdb8051659619e6e62cd93d091702a07
k2supra <andrijkorolevic@gmail.com>
2024-05-06 15:54:04 +0300

    ,

---
+++ Exceptions/ex_2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Dictionary<string, int> dict = new Dictionary<string, int>()
+            {
+                {"zero", 0 },
+                {"one", 1},
+                {"two", 2},
+                {"three", 3},
+                {"four", 4},
+                {"five", 5},
+                {"six", 6},
+                {"seven", 7},
+                {"eight", 8},
+                {"nine", 9}
+            };
+                Console.Write("Enter a number in string form: ");
+                string number = Console.ReadLine().ToLower();
+                if (!dict.ContainsKey(number))
+                {
+                    throw new ArgumentException("Wrong word/number");
+                }
+                Console.WriteLine($"Number = {dict[number]}");
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+        }
+    }
+}
---
+++ Exceptions/ex_3/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_3
+{
+    class Passport
+    {
+        private string fullname;
+        private DateTime date;
+        private string number;
+
+        public string Fullname
+        {
+            get { return fullname; }
+            set 
+            {
+                try
+                {
+                    fullname = value;
+                }
+                catch (FormatException fex)
+                {
+                    Console.WriteLine($"Error: {fex.Message}");
+                }
+            }
+        }
+        public DateTime Date
+        {
+            get { return date; }
+            set
+            {
+                try
+                {
+                    date = value;
+                }
+                catch (FormatException fex)
+                {
+                    Console.WriteLine($"Error: {fex.Message}");
+                }
+            }
+        }
+        public string Number
+        {
+            get { return number; }
+            set
+            {
+                try
+                {
+                    number = value;
+                }
+                catch (FormatException fex)
+                {
+                    Console.WriteLine($"Error: {fex.Message}");
+                }
+
+            }
+        }
+        public void display()
+        {
+            Console.WriteLine($"Name: {Fullname}");
+            Console.WriteLine($"Number: {Number}");
+            Console.WriteLine($"Date: {Date}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Passport pass = new Passport();
+            pass.Number = "6351125";
+            pass.Fullname = "Abooba Big";
+            pass.Date = DateTime.Parse("24.05.2025");
+
+            pass.display();
+        }
+    }
+}
