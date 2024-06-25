f44354383a770f1da8bbc17381805f799162bd6e
k2supra <andrijkorolevic@gmail.com>
2024-06-25 16:59:06 +0300

    .

---
+++ Files/ex_3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ex_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "test.txt";
+            string moderationfile = "moderation.txt";
+
+            string content = File.ReadAllText(file);
+            string[] mod = File.ReadAllLines(moderationfile);
+            foreach (var item in mod)
+            {
+                content = Regex.Replace(content, $@"\b{Regex.Escape(item)}\b", new string('*', item.Length), RegexOptions.IgnoreCase);
+            }
+            File.WriteAllText(file, content);
+            Console.WriteLine("Moderation completed.");
+        }
+    }
+}
