6d79c5843a2112c35fe9f29fdac5875a540f4f6c
k2supra <andrijkorolevic@gmail.com>
2024-09-23 11:25:10 +0300

    .

---
+++ DotNet/ex_6/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace ex_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            pictureBox1 = new PictureBox();
+            timer2 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(47, 43);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.satellite;
+            pictureBox1.Location = new Point(27, 195);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(164, 163);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // timer2
+            // 
+            timer2.Tick += timer2_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
---
+++ DotNet/ex_6/Form1.cs
@@ -0,0 +1,42 @@
+﻿namespace ex_6
+{
+    public partial class Form1 : Form
+    {
+
+        private DateTime newYear;
+        public Form1()
+        {
+            InitializeComponent();
+            newYear = new DateTime(DateTime.Now.Year + 1, 1, 1, 0, 0, 0);
+            timer1.Interval = 1000;
+            timer1.Tick += new EventHandler(UpdateCountdown);
+            timer1.Start();
+            timer2.Start();
+        }
+        private void UpdateCountdown(object sender, EventArgs e)
+        {
+            TimeSpan timeRemaining = newYear - DateTime.Now;
+
+            if (timeRemaining.TotalSeconds > 0)
+            {
+                label1.Text = string.Format("{0}d:{1}h:{2}m:{3}s",
+                    timeRemaining.Days, timeRemaining.Hours, timeRemaining.Minutes, timeRemaining.Seconds);
+            }
+            else
+            {
+                label1.Text = "З Новим Роком!";
+                timer1.Stop();
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            
+            if (pictureBox1.Left < 600) pictureBox1.Left += 10;
+            else
+            {
+                pictureBox1.Left = 10;
+            }
+        }
+    }
+}
